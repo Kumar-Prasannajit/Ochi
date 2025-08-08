da3847b9f2bc68d2387847bcee766e862e96e946
Kumar Prasannajit Sahu <kumarprasannajitsahu@gmail.com>
2025-08-08 12:08:36 +0530

    Update Eyes component

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -14,7 +14,7 @@ function Eyes() {
                         </div>
                     </div>
                 </div>
-                <div className='w-[15vw] h-[15vw] rounded-full bg-zinc-100 flex items-center justify-center'>
+                <div className='w-[15vw] h-[15vw] rounded-full bg-zinc-10               0 flex items-center justify-center'>
                     <div className='w-2/3 h-2/3 rounded-full bg-zinc-900 relative'>
                         <div className='line w-full h-8 absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]'>
                             <div className='w-8 h-8 rounded-full bg-zinc-100'>
